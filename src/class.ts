c089920ea8923be22cbc7e4b6eee87b6d8655322
RSamaium <sam@newbot.io>
2023-12-12 12:09:32 +0100

    class

--- src/class.ts
+++ src/class.ts
@@ -1,9 +1,38 @@
 type RoleType = 'admin' | 'member'
 
-class User {
+interface IUserAge {
+    age: number
+    setAge(newAge: number): void
+}
+
+interface IUserEmail {
+    email: string
+}
+
+interface IUserEmail {
+    setEmail(newEmail: string): void
+}
+
+interface IUser extends IUserAge, IUserEmail {
+    company?: string
+}
+
+abstract class User implements IUser {
     static url: string = '/api/users'
+    age: number = 18
+    email: string = 'test'
 
     constructor(private _name: string,  protected role: RoleType = 'member') { }
+
+    abstract setRole(): void;
+    
+    setAge(newAge: number): void {
+        this.age = newAge
+    }
+
+    setEmail(newEmail: string): void {
+        this.email = newEmail
+    }
 
     avatar(id: number): string
     avatar(folder: string): string
@@ -33,6 +62,10 @@ class Admin extends User {
         super(name, 'admin')
     }
 
+    setRole(): void {
+        this.role = 'admin'
+    }
+
     deleteUser(user: User): User {
         console.log('supprimer un utilisateur', user.name)
         return user
@@ -44,7 +77,14 @@ class Admin extends User {
     }
 }
 
+class Member extends User {
+    setRole(): void {
+        this.role = 'member'
+    }
+}
+
 const admin = new Admin('ana')
-const user = new User('ben')
-admin.deleteUser(user)
+const member = new Member('ben')
+member.setRole()
+admin.deleteUser(member)
 console.log(admin.avatar('images'))
